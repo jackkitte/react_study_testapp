9098977c6f62b81d95d10fce93acffbe6456ae7c
jackkitte <jackkitte735@gmail.com>
2019-08-16 19:13:54 +0900

    Material-UIの練習

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 import Child from './components/Child';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Divider from '@material-ui/core/Divider';
 
 class App extends Component {
   constructor(props) {
@@ -44,17 +50,23 @@ class App extends Component {
         <input value={this.state.changeResult} onChange={this.onChange.bind(this)} placeholder="change"/>{this.state.changeResult}<br/>
         <input defaultValue={this.state.inputResult} onInput={this.onInput.bind(this)} placeholder="input"/>{this.state.inputResult}<br/>
         <input defaultValue={this.state.keyupResult} onKeyUp={this.onKeyup.bind(this)} placeholder="keyup"/>{this.state.keyupResult}
-        <ul>
+        <List>
           {
             this.state.todos.map((todo) => {
               return (
-                <li key={todo.id}>{todo.text}
-                  <button onClick={this.deleteTodo.bind(this, todo.id)}>x</button>
-                </li>
+                <div>
+                  <ListItem key={todo.id}>
+                    <ListItemText>{todo.text}</ListItemText>
+                    <IconButton variant="fab" aria-label="delete" onClick={this.deleteTodo.bind(this, todo.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItem>
+                  <Divider />
+                </div>
               )
             })
           }
-        </ul>
+        </List>
       </div>
     );
   }
